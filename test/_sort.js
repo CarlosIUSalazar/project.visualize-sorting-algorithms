be83e56645fab0cfef2cb0fdc7ec394c3bf37be2
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-04-15 12:45:43 +0900

    Bubble sort animation

--- test/_sort.js
+++ test/_sort.js
@@ -5,7 +5,15 @@ describe("Sort", () => {
   it("should be a function", () => {
     expect(Sort).to.be.a("function");
   });
+
   it("should have a sort method", () => {
     expect(Sort.prototype.sort).to.be.a("function");
+  });
+
+  it("should return a sorted version of input array", () => {
+    let array = [6, 4, 9, 23, 5, 4, 77, 5, 1];
+    let bubbleSort = new Sort();
+    //console.log("Sorted", bubbleSort.sort(array));
+    expect(bubbleSort.sort(array)).to.deep.equal([1, 4, 4, 5, 5, 6, 9, 23, 77]);
   });
 });
